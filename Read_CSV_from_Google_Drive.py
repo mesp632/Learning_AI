0f1e835b37619485d031fa0f098ba9b1ea5260ce
mesp632 <152715449+mesp632@users.noreply.github.com>
2024-11-30 08:49:43 -0700

    Read CSV From google drive
    Use pandas to read CSV and gdown module to download fields from google drive. Reads out parts of the data in the file.

---
+++ Read_CSV_from_Google_Drive.py
@@ -0,0 +1,43 @@
+#import pandas module to read the CSV file
+import pandas as pd
+
+#import gdown module to download fields from google drive
+import gdown
+
+url="https://drive.google.com/file/d/1sQGqysf7HFPngnKgumOlzSmyI0CHgGkF/view"
+
+#Derive the file id from the url
+file_id = url.split('/')[-2]
+
+#Derive the download url of the file
+download_url = 'https://drive.google.com/uc?id=' + file_id
+
+#save the file name
+file_name = "child_vs_adult.csv"
+
+#Derive file location
+file_location = file_name
+
+#Download the file from drive
+gdown.download(download_url, file_location, quiet=False)
+
+#Read CSV File
+data = pd.read_csv(file_location)
+
+#Print a sample (first row) from the tabular data
+print('---- The first row from the data set ----')
+print(data.head(1))
+
+#See the column names in the table
+column_names = data.columns
+print('---- Column Names in Table ----')
+for column in column_names:
+    print(column)
+
+#change the column name
+column_name = "height"
+
+#Print specific column
+print('---- Data from Column ----')
+column = data[[column_name]]
+print(column)
